d378b3e812a29248990395a2fc8b73ce8a6d217a
ferhanmm <ferhanmm@gmail.com>
2024-09-27 15:38:44 -0500

    header,body,footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from '../../components/header/page';
+import Body from '../../components/body/page';
 import Footer from '../../components/footer/page';
 
 export default function Page() {
     return (
         <div>
             <Header />
-            <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+            <Body />
             <Footer />
         </div>
     )
