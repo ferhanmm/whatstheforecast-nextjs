79b492c462c6c49aaa1e269ac1bbbb069ea1712d
ferhanmm <ferhanmm@gmail.com>
2024-09-27 19:22:51 -0500

    updating img to Image

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -30,9 +30,11 @@ export default function Header() {
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
                 <Image
-                  alt="Your Company"
+                  alt="Tailwind CSS"
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                   className="h-8 w-auto"
+                  height={80}
+                  width={80}
                 />
               </div>
               <div className="hidden sm:ml-6 sm:block">
@@ -73,6 +75,8 @@ export default function Header() {
                       alt=""
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       className="h-8 w-8 rounded-full"
+                      height="80"
+                      width="80"
                     />
                   </MenuButton>
                 </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
+const nextConfig = {
+    images: {
+      domains: ['tailwindui.com', 'images.unsplash.com'],
+      dangerouslyAllowSVG: true,
+    },
+  };
+  
 export default nextConfig;
