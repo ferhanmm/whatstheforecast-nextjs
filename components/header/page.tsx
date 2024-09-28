dda01af6a7c7520e114c05ebac6682b0d5cb8a2b
ferhanmm <ferhanmm@gmail.com>
2024-09-27 19:23:14 -0500

    Update page.tsx

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -75,8 +75,8 @@ export default function Header() {
                       alt=""
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       className="h-8 w-8 rounded-full"
-                      height="80"
-                      width="80"
+                      height={80}
+                      width={80}
                     />
                   </MenuButton>
                 </div>
