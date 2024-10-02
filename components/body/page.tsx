3d4ffa5ae0a08db6328d823de7071bac5cc59737
ferhanmm <ferhanmm@gmail.com>
2024-10-01 23:37:00 -0500

    Update page.tsx

--- components/body/page.tsx
+++ components/body/page.tsx
@@ -36,9 +36,9 @@ export default function Body() {
         <div className="h-32 rounded-lg bg-gray-200"></div>
         <div className="flex justify-center items-center h-32 rounded-lg bg-gray-200 lg:col-span-2 ">
           
-          <div className="flex flex-col items-center justify-center min-h-screen">
-          <h1>Testing</h1>
-          <Submitbutton />
+          <div className="flex flex-col items-center">
+            <h1>Testing</h1>
+            <Submitbutton />
           </div>
 
         </div>
