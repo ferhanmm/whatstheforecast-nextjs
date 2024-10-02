1e40f5581560fd889465d0fa17f76e732bd3053b
ferhanmm <ferhanmm@gmail.com>
2024-10-01 23:43:08 -0500

    searchbar

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,3 +1,43 @@
+export function Searchbar() {
+  return(
+    <div className="relative">
+      <label htmlFor="Search" className="sr-only"> Search for... </label>
+
+      <input
+        type="text"
+        id="Search"
+        placeholder="chad@rhcp.com"
+        className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white"
+      />
+
+      <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+        <button
+          type="button"
+          className="text-gray-600 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+        >
+          <span className="sr-only">Search</span>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </span>
+    </div>
+
+  );
+}
+
 export default function Header() {
   return (
     <header className="bg-white dark:bg-gray-900">
@@ -15,64 +55,8 @@ export default function Header() {
           </a>
         </div>
   
-        <div className="hidden md:block">
-          <nav aria-label="Global">
-            <ul className="flex items-center gap-6 text-sm">
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  About
-                </a>
-              </li>
-  
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Careers
-                </a>
-              </li>
-  
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  History
-                </a>
-              </li>
-  
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Services
-                </a>
-              </li>
-  
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Projects
-                </a>
-              </li>
-  
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Blog
-                </a>
-              </li>
-            </ul>
-          </nav>
+        <div>
+          <Searchbar/>
         </div>
   
         <div className="flex items-center gap-4">
